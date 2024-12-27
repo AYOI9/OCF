1fb78525046760b80e12f06246c8ced46fba0332
AYOI9 <maiorovevgen25@gmail.com>
2024-12-27 11:13:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+        int maxDigit = 0;
+        for (int i = 10; i <= N; i++)
+        {
+            int number = i;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit > maxDigit)
+                {
+                    maxDigit = digit;
+                }
+                number /= 10;
+            }
+        }
+
+        Console.WriteLine(maxDigit);
